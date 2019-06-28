b17e5d0f01611aeed8996e67d7a24b341346976c
Montandalar <jbis1337@hotmail.com>
2019-06-28 14:26:28 +1000

    Initial commit: dependencies, description, stub code

---
+++ init.lua
@@ -0,0 +1,7 @@
+if (minetest.get_modpath("ethereal")) then
+    minetest.log("action", ethereal detected)
+end
+
+if (minetest.get_modpath("stairs")) then
+    minetest.log("action", "stairs detected")
+end
